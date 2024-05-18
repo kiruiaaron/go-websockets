ca77325e246a03bd1ca985cf51c05011dcc937d7
kiruiaaron <kiruiaaron471@gmail.com>
2024-05-18 17:55:49 +0300

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kiruiaaron/go-websockets
+
+go 1.22.0
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "net/http"
+
+
+func main(){
+    setupAPI()
+}
+
+func setupAPI(){
+	http.Handle("/",http.FileServer(http.Dir("./frontend")))
+}
